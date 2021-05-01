5217da103d07fc8fa9d77ae0bc7b4e8a06630274
Kaustubh Deshpande <kaustubhdeshpande@Kaustubhs-MacBook-Pro.local>
2021-04-30 21:14:31 -0700

    "Initialize Repository"

---
+++ datatools.py
@@ -0,0 +1,43 @@
+# import gpiozero
+
+class Microphone(object):
+    def __init__(self, time, status):
+        self.time = time
+        self.status = status
+
+    def print_time(self):
+        print(self.time)
+        print(self.status)
+
+
+class Button(object):
+    def __init__(self, status):
+        self.time = time
+        self.status = False
+
+
+
+# #Object : Microphone
+# Time 
+# Frequency (lc,hc)
+# Measurement 
+# Processsing
+# Status: False
+# if TImer == 0 .. set True
+
+
+# #Button Object 
+# Boolean (Button) True or False
+# Track status of boolean
+
+
+# While at least one microphone False:
+#     Keep going 
+
+# If all microphones True:
+#     terminate 
+
+
+
+
+
---
+++ pipeline.py
@@ -0,0 +1,44 @@
+from datatools import Microphone
+
+#instantiate microphones
+upper_right_lung = Microphone()
+lower_right_lung = Microphone()
+upper_left_lung = Microphone()
+lower_left_lung = Microphone()
+mitrial_valve = Microphone()
+tricuspid_valve = Microphone()
+pulmonary_valve = Microphone()
+aortic_valve = Microphone()
+
+
+
+
+# While True:
+#             iF button pressed:
+#                                 .
+#                                 .
+#                                 .
+#                                 .
+#                                 ..
+#                                 .
+#                                 .save as upper lung x to SD
+
+#                                 .
+#                                 .save as lower lung x  to sd
+#                                 .
+#                                 .
+#                                 .
+#                                 .
+#                                 .
+
+#                                 .
+
+#                                 .all 8 recordings finished 
+#             .
+#             .
+#             .
+#             .
+#             .
+#             .
+#             .
+#                                 save
