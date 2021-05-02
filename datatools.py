ee36c0229abe2ac7c024d046205d7582a2e21b54
rahuluppuluri <rtuppuluri@ucdavis.edu>
2021-05-02 14:00:15 -0700

    added async

--- datatools.py
+++ datatools.py
@@ -2,7 +2,7 @@
 from scipy.signal import butter, lfilter
 
 class Microphone(object):
-    def __init__(self, time: int, lc: int, hc: int, fs: int, order: int,  pin: int, status = False)
+    def __init__(self, time: int, lc: int, hc: int, fs: int, order: int,  pin: int, status = False):
         self.time = time
         self.pin = pin
         self.status = status
@@ -23,14 +23,17 @@ class Microphone(object):
         y = lfilter(b, a, data)
         return y
 
-    def record(self):
+    async def record(self):
         # print(self.pin)
         # print(self.status)
+        print("record")
+        return
+
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.pin, GPIO.IN)
         timer = self.time
         
-        while timer > 0
+        while timer > 0:
             if GPIO.input(self.pin):
                 # b, a = signal.butter(4, 100, 'low', analog=True)
                 # w, h = signal.freqs(b, a)
@@ -42,20 +45,22 @@ class Microphone(object):
 
 
 
-    T = 0.05
-    nsamples = T * fs
-    t = np.linspace(0, T, nsamples, endpoint=False)
-    a = 0.02
-    f0 = 600.0
-    x = 0.1 * np.sin(2 * np.pi * 1.2 * np.sqrt(t))
-    x += 0.01 * np.cos(2 * np.pi * 312 * t + 0.1)
-    x += a * np.cos(2 * np.pi * f0 * t + .11)
-    x += 0.03 * np.cos(2 * np.pi * 2000 * t)
-    plt.figure(2)
-    plt.clf()
-    plt.plot(t, x, label='Noisy signal')
+    # T = 0.05
+    # nsamples = T * fs
+    # t = np.linspace(0, T, nsamples, endpoint=False)
+    # a = 0.02
+    # f0 = 600.0
+    # x = 0.1 * np.sin(2 * np.pi * 1.2 * np.sqrt(t))
+    # x += 0.01 * np.cos(2 * np.pi * 312 * t + 0.1)
+    # x += a * np.cos(2 * np.pi * f0 * t + .11)
+    # x += 0.03 * np.cos(2 * np.pi * 2000 * t)
+    # plt.figure(2)
+    # plt.clf()
+    # plt.plot(t, x, label='Noisy signal')
+    #
+    # y = butter_bandpass_filter(x, lowcut, highcut, fs, order=6)
 
-    y = butter_bandpass_filter(x, lowcut, highcut, fs, order=6)
+
 
     # def print_time(self):
     #     print(self.time)
