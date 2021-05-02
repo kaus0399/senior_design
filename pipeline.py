70d62dd55e7676e60dcc16e8b2aa0578bbe0cab1
rahuluppuluri <rtuppuluri@ucdavis.edu>
2021-05-01 21:58:33 -0700

    added filters

--- pipeline.py
+++ pipeline.py
@@ -7,14 +7,14 @@ from datatools import *
 # while True:
 #     if GPIO.input(10) == GPIO.HIGH:
         #instantiate microphones
-upper_right_lung = Microphone(time=30, lc=60, hc=1200, pin=1)
-lower_right_lung = Microphone(time=30, lc=60, hc=1200, pin=2) 
-upper_left_lung = Microphone(time=30, lc=60, hc=1200, pin=3)
-lower_left_lung = Microphone(time=30, lc=60, hc=1200, pin=4)
-mitrial_valve = Microphone(time=30, lc=60, hc=1200, pin=5)
-tricuspid_valve = Microphone(time=30, lc=60, hc=1200, pin=6)
-pulmonary_valve = Microphone(time=30, lc=60, hc=1200, pin=7)
-aortic_valve = Microphone(time=30, lc=60, hc=1200, pin=8)
+upper_right_lung = Microphone(time=30, lc=60, hc=1200, pin=1, fs=1000, order=)
+lower_right_lung = Microphone(time=30, lc=60, hc=1200, pin=2, fs=1000, order=)
+upper_left_lung = Microphone(time=30, lc=60, hc=1200, pin=3, fs=1000)
+lower_left_lung = Microphone(time=30, lc=60, hc=1200, pin=4, fs=1000)
+mitrial_valve = Microphone(time=30, lc=20, hc=500, pin=5, fs=1000)
+tricuspid_valve = Microphone(time=30, lc=20, hc=500, pin=6, fs=1000)
+pulmonary_valve = Microphone(time=30, lc=20, hc=500, pin=7, fs=1000)
+aortic_valve = Microphone(time=30, lc=20, hc=500, pin=8, fs=1000)
 
 
 upper_left_lung.record()
